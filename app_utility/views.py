8d659779ea3ad2f3636732a2a21330de93138add
ArunD <arun.dakua@gmail.com>
2021-01-14 15:07:15 +0530

    lexer feature

--- app_utility/views.py
+++ app_utility/views.py
@@ -8,6 +8,7 @@ from TextUtility.settings import FILE_DIR
 from app_utility.parser.wordCount import TextParser
 from app_utility.parser.specialChar import SpecialCharParser
 from app_utility.parser.removeChar import RemoveCharParser
+from app_utility.parser.lexerHtml import LexerOp
 
 
 # Create your views here.
@@ -21,39 +22,44 @@ def perform_utility(request,utility_type):
     id = params['id']
     id = str(id)
     
-
+    #extn = params['extn']
+    extn = '.txt'
+    fname = params['fname']
     if utility_type == "textstats/":
         opdir = "/textStatsData/"
         parserop = TextParser()
     elif utility_type == "removechar/":
         opdir = "/removeCharData/"
         remove_char = params['remove_char']
-        parserop = RemoveCharParser()
-    else:
+        parserop = RemoveCharParser(remove_char)
+    elif utility_type == "specialchar/":
         opdir = "/specialCharData/"
         parserop = SpecialCharParser()
+    elif utility_type == "lexer/":
+        opdir = "/lexerOpData/"
+        language = params['language']
+        parserop = LexerOp(language)
+        extn = '.html'
 
     try:
         os.mkdir(FILE_DIR+'/user_'+id +opdir)
     except Exception as err:
         print('Mkdir Error : {0}'.format(err))
 
-    print(parserop)
+    #print(parserop)
     if 'file_name' in params:
         document = params['file_name']
         uploaded_path = os.path.join(FILE_DIR,'user_'+id +'/rawData/' + document)
-
-        #textStats_path = os.path.join(FILE_DIR,'user_'+id +'/textStatsData/' + document)
-        textStats_path = os.path.join(FILE_DIR,'user_'+id +opdir + document)
         datatype = 'file'
-        textDict = parserop.parse(datatype,uploaded_path,remove_char)
+        textDict = parserop.parse(datatype,uploaded_path)
     else :
         text_upload = params['text_upload']
         fname = params['fname']
         datatype = 'notfile'
-        textStats_path = os.path.join(FILE_DIR,'user_'+id +opdir + fname + '.txt')
-        textDict = parserop.parse(datatype,text_upload,remove_char)
+        textDict = parserop.parse(datatype,text_upload)
     
+    
+    textStats_path = os.path.join(FILE_DIR,'user_'+id +opdir + fname + extn)
     #print(uploaded_path)
     
     wf = open(textStats_path,'w')
@@ -73,10 +79,12 @@ def perform_utility(request,utility_type):
                     print(val[0] +' : ' + str(val[1]),file=wf)
     elif utility_type == "removechar/":
         print(textDict['removeChar'],file=wf)
-    else :
+    elif utility_type == "specialchar/":
         print(textDict['specialChar'],file=wf)
-        textDict['status'] = 200
-
+    elif utility_type == "lexer/":
+        print(textDict['lexerOp'],file=wf)
+        
+    textDict['status'] = 200
     wf.close
 
     return JsonResponse(data=textDict)
